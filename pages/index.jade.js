f7384bd4712f9fed0c631948383152485666372c
Vitaliy Filipov <altras@gmail.com>
2013-07-05 17:48:58 +0300

    styled the login button

--- pages/index.jade.js
+++ pages/index.jade.js
@@ -2,13 +2,11 @@ var boot = require("../client/boot");
 
 
 $(document).ready(function() {
-  console.log('before if');
   if (!window.WebGLRenderingContext) {
     // the browser doesn't even know what WebGL is
     window.location = "http://get.webgl.org";
   } else {
     var canvas = document.getElementById("myCanvas");
-        console.log(canvas);
     var ctx = canvas.getContext("webgl");
     if (!ctx) {
       // browser supports WebGL but initialization failed.
