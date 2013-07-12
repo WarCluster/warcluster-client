fd6149c3fa8ac6250c9c665664ca335fe4f8b4f0
Vitaliy Filipov <altras@gmail.com>
2013-07-12 20:01:42 +0300

    added uservoice

--- pages/game/index.jade.js
+++ pages/game/index.jade.js
@@ -42,5 +42,20 @@ $(document).ready(function() {
     console.log(battleField.playerData)
     battleField.attack(battleField.playerData.HomePlanet.id, battleField.playerData.PlanetToAttack.id);
   });
+  // USERVOICE widget
+  (function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/ZD3yBKaqsWuw05GqkIQmyQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+  UserVoice = window.UserVoice || [];
+  UserVoice.push(['showTab', 'classic_widget', {
+    mode: 'full',
+    primary_color: '#f69666',
+    link_color: '#f1485e',
+    default_mode: 'feedback',
+    forum_id: 214551,
+    tab_label: 'Feedback & Support',
+    tab_color: '#a9466f',
+    tab_position: 'middle-right',
+    tab_inverted: false
+  }]);
+  // end of UserVoice widget
 });
 
