adffa21f265d5b61e5834f6acb6b9fe0a942d70b
Pavel Dobrev <pavel@camplight.net>
2013-08-21 13:09:41 +0300

    fix refactor parsing and fix timing

--- client/warcluster/space-objects/ships/Ship.js
+++ client/warcluster/space-objects/ships/Ship.js
@@ -32,13 +32,13 @@ module.exports.prototype.prepare = function(total) {
 module.exports.prototype.send = function(mission) {
 	this.mission = mission;
 
-	this.delta_x = this.mission.target.x - this.mission.source.x;
-	this.delta_y = this.mission.target.y - this.mission.source.y;
+	this.delta_x = this.mission.Target[0] - this.mission.Source[0];
+	this.delta_y = this.mission.Target[1] - this.mission.Source[1];
 	
 	this.rotation.z = -Math.atan2(this.delta_x, this.delta_y) + Math.PI;
 	this.ship.rotation.y = Math.PI * Math.random();
 
-	this.endTime = this.mission.startTime + this.mission.travelTime;
+	this.endTime = this.mission.StartTime + this.mission.TravelTime;
 
 	this.ship.position.x = this.formation.x;
 	this.ship.position.y = this.formation.y;
@@ -49,13 +49,13 @@ module.exports.prototype.tick = function() {
 	if (this.context.currentTime > this.endTime) {
 		this.context.shipsFactory.destroy(this);
 	} else {
-		this.progress = (this.context.currentTime - this.mission.startTime) / this.mission.travelTime;
+		this.progress = (this.context.currentTime - this.mission.StartTime) / this.mission.TravelTime;
 
 		if (this.progress > 1)
 			this.progress = 1;
 
-		this.position.x = this.mission.source.x + this.delta_x * this.progress;
-		this.position.y = this.mission.source.y + this.delta_y * this.progress;
+		this.position.x = this.mission.Source[0] + this.delta_x * this.progress;
+		this.position.y = this.mission.Source[1] + this.delta_y * this.progress;
 
 		this.angle += this.direction;
 
