e89bb3a03afdd0588f7cbd33278633a3ce879667
Vitaliy Filipov <altras@gmail.com>
2013-12-03 19:31:15 +0200

    fixed the zooming navigation

--- client/warcluster/controllers/view/scroller/index.js
+++ client/warcluster/controllers/view/scroller/index.js
@@ -11,9 +11,6 @@ module.exports = function(context, config) {
   this.yMin = config.yMin || -4000000;
   this.yMax = config.yMax || 4000000;
   this.scaleIndex = 1;
-  // this.uselessFlag = false;
-
-  // *****************************************************************
 
   var self = this;
   var scrollMouseUp = function(e) {
@@ -27,11 +24,9 @@ module.exports = function(context, config) {
     var dy = self.scrollPositon.y + (e.clientY * self.scaleIndex - self.mpos.y);
 
     self.setScrollPosition(dx, dy);
-    // console.log("mpos before: " + self.mpos.x + "," + self.mpos.y);
+
     self.mpos.x = e.clientX * self.scaleIndex;
     self.mpos.y = e.clientY * self.scaleIndex;
-    // console.log("mpos after: " + self.mpos.x + "," + self.mpos.y);
-    console.log("-----scrollPosition: " + self.scrollPositon.x + "," + self.scrollPositon.y);
 
     TweenLite.to(self.context.spaceScene.camera.position, 0.7, {
       x: -self.scrollPositon.x, 
@@ -45,6 +40,7 @@ module.exports = function(context, config) {
       }
     });
   }
+
   this.setScrollPosition = function(dx, dy) {
      if (dx < self.xMin)
       self.scrollPositon.x = self.xMin;
@@ -63,12 +59,7 @@ module.exports = function(context, config) {
   
   this.scrollMouseDown = function(e) {
     e.preventDefault();
-    // if (self.uselessFlag) {
-    //   self.context.spaceScene.camera.position.y = -self.context.spaceScene.camera.position.y;
-    // }
-    console.log("spaceScene.camera MouseMove: " + self.context.camera.position.x + "," + self.context.spaceScene.camera.position.y);
-    console.log("camera MouseMove: " + self.context.spaceScene.camera.position.x + "," + self.context.spaceScene.camera.position.y);
-    
+
     self.mpos.x = e.clientX * self.scaleIndex;
     self.mpos.y = e.clientY * self.scaleIndex;
 
@@ -80,59 +71,34 @@ module.exports = function(context, config) {
 }
 
 module.exports.prototype = new THREE.EventDispatcher();
+
 module.exports.prototype.setPosition = function (x, y) {
   this.scrollPositon.x = -x;
   this.scrollPositon.y = y;
   this.context.camera.position.x = x;
   this.context.camera.position.y = y;
 }
-//TODO: refactor according to DRY principle
+
 module.exports.prototype.scrollToMousePosition = function(xPos, yPos){
   var self = this;
 
   var windowCenterY = $(window).scrollTop() + $(window).height() / 2;
   var windowCenterX = $(window).scrollLeft() + $(window).width() / 2;
   var factor = (this.context.spaceViewController.zoomer.zoom > 84000) ? 13 : 7;
-  var dx = self.scrollPositon.x + (xPos * self.scaleIndex - windowCenterX * self.scaleIndex)/factor;
-  var dy = self.scrollPositon.y + (yPos * self.scaleIndex - windowCenterY * self.scaleIndex)/factor;
+  var dx = self.scrollPositon.x + (windowCenterX * self.scaleIndex - xPos * self.scaleIndex)/factor;
+  var dy = self.scrollPositon.y + (windowCenterY * self.scaleIndex - yPos * self.scaleIndex)/factor;
 
   self.setScrollPosition(dx, dy);
 
-  // self.mpos.x = xPos * self.scaleIndex;
-  // self.mpos.y = yPos * self.scaleIndex;
-  console.log("camera before ZoomIn: " + self.context.spaceScene.camera.position.x + "," + self.context.spaceScene.camera.position.y);
-  console.log("scroll before ZoomIn: " + self.scrollPositon.x + "," + self.scrollPositon.y);
-  self.context.spaceScene.camera.position.x = -self.scrollPositon.x;
   TweenLite.to(self.context.spaceScene.camera.position, 0.5, {
-    x: self.scrollPositon.x, 
-    y: -self.scrollPositon.y,
+    x: -self.scrollPositon.x, 
+    y: self.scrollPositon.y,
     ease: Cubic.easeOut,
     onUpdate: function() {
       self.dispatchEvent({
         type: "scroll", 
         objects: self.selectedPlanets
       });
-    },
-    onComplete: function(){
-      debugger;
-      console.log("spaceScene.camera after ZoomIn: " + self.context.spaceScene.camera.position.x + "," + self.context.spaceScene.camera.position.y);
-      console.log("camera after ZoomIn: " + self.context.camera.position.x + "," + self.context.camera.position.y);
-      
-      console.log("scroll after ZoomIn: " + self.scrollPositon.x + "," + self.scrollPositon.y);
-      // self.setPosition(self.scrollPositon.x,-self.scrollPositon.y);
-      // self.context.spaceScene.camera.position.x = self.scrollPositon.x;
-      // self.context.spaceScene.camera.position.y = -self.scrollPositon.y;
-      self.scrollPositon.y = self.context.camera.position.y;
-      // self.context.spaceScene.camera.position.y = self.scrollPositon.y;
-      
-      // self.uselessFlag = true;
-      // self.scrollPositon.x = -self.scrollPositon.x;
-      // self.scrollPositon.y = self.scrollPositon.y;
-      console.log("spaceScene.camera position after ZoomIn: " + self.context.spaceScene.camera.position.x + "," + self.context.spaceScene.camera.position.y);
-      console.log("camera position after ZoomIn: " + self.context.camera.position.x + "," + self.context.camera.position.y);
-      
-      console.log("scroll position after ZoomIn: " + self.scrollPositon.x + "," + self.scrollPositon.y);
-      console.log("------------------------------------- end--------------------");
     }
   });
   
