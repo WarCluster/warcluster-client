bc0e59e03cbdea363e60c43a470218343cd2d16a
Vitaliy Filipov <altras@gmail.com>
2014-01-25 22:28:00 +0200

    delete comments and debugger

--- client/warcluster/controllers/view/zoomer/index.js
+++ client/warcluster/controllers/view/zoomer/index.js
@@ -11,7 +11,6 @@ module.exports = function(context, config){
   this.mousePosition = { x: 0, y: 0};
 
   this.zoomIt = function(e) {
-    debugger;
     var zoomMode, step,
         self = this;
     if (e === "in" || e === "out") {
@@ -72,54 +71,6 @@ module.exports = function(context, config){
   $(window).mousewheel(function(e){
     self.zoomIt(e);
   });
-    // function(e) {
-  //   e.preventDefault();
-
-  //   var step = e.deltaY > 0 ? -self.zoomStep : self.zoomStep;
-  //   var zoomMode = e.deltaY > 0 ? "zoomin" : "zoomout";
-  //   self.mousePosition.x = (zoomMode === "zoomin") ? e.clientX : 0;
-  //   self.mousePosition.y = (zoomMode === "zoomin") ? e.clientY : 0;
-
-  //   if (self.minZoom != null && self.maxZoom != null) {
-  //     if (self.zoom + step < self.minZoom)
-  //       self.zoom = self.minZoom;
-  //     else if (self.zoom + step > self.maxZoom)
-  //       self.zoom = self.maxZoom;
-  //     else
-  //       self.zoom += step;
-  //   } else if (self.minZoom != null && self.maxZoom == null) {
-  //     if (self.zoom + step < self.minZoom)
-  //       self.zoom = self.minZoom;
-  //     else
-  //       self.zoom += step;
-  //   } else if (self.minZoom == null && self.maxZoom != null) {
-  //     if (self.zoom + step > self.maxZoom)
-  //       self.zoom = self.maxZoom;
-  //     else
-  //       self.zoom += step;
-  //   } else {
-  //     self.zoom += step;
-  //   }
-
-  //   TweenLite.to(self.context.spaceScene.camera.position, 0.5, {
-  //     z: self.zoom,
-  //     ease: Cubic.easeOut,
-  //     onStart: function() {
-  //       self.dispatchEvent({
-  //         type: "zoom", 
-  //         zoom: self.zoom,
-  //         mode: zoomMode
-  //       });
-  //     },
-  //     onComplete: function(){        
-  //       self.dispatchEvent({
-  //         type: "scopeOfView", 
-  //         zoom: self.zoom
-  //       }); 
-  //     }
-  //   });
-  // }
-  // );
 }
 
 module.exports.prototype = new THREE.EventDispatcher();
