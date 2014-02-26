3959ae0d784c60c70d5f5e75a2437cb1670c78d5
Vitaliy Filipov <altras@gmail.com>
2014-02-26 02:07:36 +0200

    pick sun and race

--- client/warcluster/views/landing/index.js
+++ client/warcluster/views/landing/index.js
@@ -6,13 +6,17 @@ module.exports = Backbone.View.extend({
   template: jadeCompile(require("./index.jade")),
   events: {
     "click .start-game": "startGame",
-    "click .race": "selectRace"
+    "click .race": "selectRace",
+    "click .sun": "selectSun"
     // "click .toggle-leaderboard-btn": "showLeaderboard"
   },
   className: "landing-view container text-center",
   initialize: function(context) {
     this.context = context;
     this.twitter = context.playerData.twitter;
+
+    this.selectedRace = -1;
+    this.selectedSun = -1;
   },
   renderStatistics: function() {
     this.$el.html(this.template({twitter: this.twitter}));
@@ -31,13 +35,30 @@ module.exports = Backbone.View.extend({
   },
   startGame: function() {
     if ($(".start-game").html() === "Start Game") {
-      this.context.commandsManager.setupParameters(0, 1);
+      if ((this.selectedRace > -1 && this.selectedRace < 6) && (this.selectedSun > -1 && this.selectedSun < 2)) {
+        this.context.commandsManager.setupParameters(this.selectedRace, this.selectedSun);
+      } else {
+        alert("You must pick both your race and your sun in order to continue");
+        return;
+      }
     }
     $(".landing-view").remove();
     delete this.leaderboard;
     // router.navigate("battle-field", true)
   },
   selectRace: function(e) {
+    debugger;
+    if ($(e.currentTarget).attr("data-id") > 3) return; //they're locked
+    $(e.currentTarget).parent().find(".selected").removeClass("selected");
+    $(e.currentTarget).find(".race-color").addClass("selected");
+    this.selectedRace = $(e.currentTarget).attr("data-id");
     // $(e.currentTarget).find(".race-color").css({"border-bottom": "5px outset #f26a21"});  }
+  },
+  selectSun: function(e) {
+    debugger;
+    if ($(e.currentTarget).attr("data-id") > 1) return; //they're locked
+    $(e.currentTarget).parent().find(".selected").removeClass("selected");
+    $(e.currentTarget).find("img").addClass("selected");
+    this.selectedSun = $(e.currentTarget).attr("data-id");   
   }
 })
