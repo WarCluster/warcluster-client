88127b5c4f16d85a50d4309ea90d25b04cf462ae
Vitaliy Filipov <altras@gmail.com>
2014-04-02 21:13:13 +0300

    add poll and populate the individual leaderboard page

--- client/warcluster/views/leaderboard/index.js
+++ client/warcluster/views/leaderboard/index.js
@@ -5,7 +5,8 @@ module.exports = Backbone.View.extend({
   template: jadeCompile(require("./index.jade")),
   events: {
     "click #individualBtn": "showIndividualLeaderboard",
-    "click #teamBtn": "showTeamLeaderboard"
+    "click #teamBtn": "showTeamLeaderboard",
+    "click .race-color": "showTeamLeaderboard"
   },
   className: "leaderboard-content",
   initialize: function() {
@@ -36,80 +37,48 @@ module.exports = Backbone.View.extend({
     }
   },
   connectIndividualLeaderboard: function(){
-    // clearTimeout(self.t);
-    $.ajax({
-        url: self.config.ajaxUrl + "/players/?page=1", 
-        dataType: 'json',
-        statusCode: {
-          404: function () {
-            //TODO:
-            //not enough players, maybe go to page 1?;
+    clearTimeout(self.t);
+    poll = function() {
+      $.ajax({
+          url: self.config.ajaxUrl + "/players/?page=1", 
+          dataType: 'json',
+          statusCode: {
+            404: function () {
+              //TODO:
+              //not enough players, maybe go to page 1?;
+              console.log("Page Not Found");
+              $("#individual").html("Woops, we didn't have time to handle this error right. Sorry for the inconvinience! Why don't you refresh? :) ");
+            },
+            400: function() {
+              //TODO:
+              //bad request, maybe go to page 1?;
+              $("#individual").html("Woops, we didn't have time to handle this error right. Sorry for the inconvinience! Why don't you refresh? :) ");
+            }
           },
-          400: function() {
-            //TODO:
-            //bad request, maybe go to page 1?;
-          }
-        },
-        success: function(data) {
-          debugger;
-          for(i=0;i<data.length;i++) {
-            $("tbody tr:nth-child(" + 1 + ")").html("<td>1</td><td><a href='http://twitter.com/" + data[i].Username + "'>" + data[i].Username + "</td><td>" + data[i].Team + "</td><td>" + data[i].HomePlanet + "</td><td>" + data[i].Planets + "</td><td>");
-            // if ($('#user-'+data[i].id).length == 0) {
-            //   // this id doesn't exist, so add it to our list.
-            //   $("#leaderboard").append('<li><h1 style="display:inline" id="user-' + data[i].id + '">' + data[i].score + '</h1> <img style="height:50px" src="http://graph.facebook.com/' + data[i].facebook_id + '/picture"/> ' + data[i].username + '</li>');
-            // } else {
-            //   // this id does exist, so update 'score' count in the h1 tag in the list item.
-            //   $('#user-'+data[i].id).html(data[i].score);
-            // }
-          }
-          // sort();
-        },
-      }); 
-    // function poll() {
-    //   $.ajax({
-    //     url: self.config.ajaxUrl, // needs to return a JSON array of items having the following properties: id, score, facebook_id, username
-    //     dataType: 'json',
-    //     success: function(o) {
-    //       debugger;
-    //       for(i=0;i<o.length;i++) {
-    //         if ($('#user-'+o[i].id).length == 0) {
-    //           // this id doesn't exist, so add it to our list.
-    //           $("#leaderboard").append('<li><h1 style="display:inline" id="user-' + o[i].id + '">' + o[i].score + '</h1> <img style="height:50px" src="http://graph.facebook.com/' + o[i].facebook_id + '/picture"/> ' + o[i].username + '</li>');
-    //         } else {
-    //           // this id does exist, so update 'score' count in the h1 tag in the list item.
-    //           $('#user-'+o[i].id).html(o[i].score);
-    //         }
-    //       }
-    //       sort();
-    //     },
-    //   }); 
-    // poll();
-    // // play it again, sam
-    // self.t=setTimeout("poll()",3000);
-    // }
+          success: function(data) {
+            for(i=0;i<data.length;i++) {
+              $("tbody tr:nth-child(" + (i+1) + ") > td:nth-child(2)").html("<a href='https://twitter.com/"+data[i].Username+"' target='_blank'>@"+data[i].Username+"</a>");
+              $("tbody tr:nth-child(" + (i+1) + ") > td:nth-child(3)").css({"background": "rgb("+ parseInt(data[i].Team.R*255)+","+parseInt(data[i].Team.G*255) +","+parseInt(data[i].Team.B*255)+")"});
+              $("tbody tr:nth-child(" + (i+1) + ") > td:nth-child(4)").html(data[i].HomePlanet);
+              $("tbody tr:nth-child(" + (i+1) + ") > td:nth-child(5)").html(data[i].Planets);
+              // $("tbody tr:nth-child(" + 1 + ")").html("<td>1</td><td><a href='http://twitter.com/" + data[i].Username + "'>" + data[i].Username + "</td><td>" + data[i].Team + "</td><td>" + data[i].HomePlanet + "</td><td>" + data[i].Planets + "</td><td>");
+              // if ($('#user-'+data[i].id).length == 0) {
+              //   // this id doesn't exist, so add it to our list.
+                // $("#leaderboard").append('<li><h1 style="display:inline" id="user-' + data[i].id + '">' + data[i].score + '</h1> <img style="height:50px" src="http://graph.facebook.com/' + data[i].facebook_id + '/picture"/> ' + data[i].username + '</li>');
+              // } else {
+              //   // this id does exist, so update 'score' count in the h1 tag in the list item.
+              //   $('#user-'+data[i].id).html(data[i].score);
+              // }
+            }
+            // sort();
+            self.t = setTimeout(poll, 2500);
+          },
+        });
+      }
+      poll();
   },
   connectTeamLeaderboard: function(){
-    // clearTimeout(self.t);
-    // function poll() {
-    //   $.ajax({
-    //     url: self.config.ajaxUrl, // needs to return a JSON array of items having the following properties: id, score, facebook_id, username
-    //     dataType: 'json',
-    //     success: function(o) {
-    //       for(i=0;i<o.length;i++) {
-    //         if ($('#user-'+o[i].id).length == 0) {
-    //           // this id doesn't exist, so add it to our list.
-    //           $("#leaderboard").append('<li><h1 style="display:inline" id="user-' + o[i].id + '">' + o[i].score + '</h1> <img style="height:50px" src="http://graph.facebook.com/' + o[i].facebook_id + '/picture"/> ' + o[i].username + '</li>');
-    //         } else {
-    //           // this id does exist, so update 'score' count in the h1 tag in the list item.
-    //           $('#user-'+o[i].id).html(o[i].score);
-    //         }
-    //       }
-    //       sort();
-    //     },
-    //   }); 
-
-    //   // play it again, sam
-    //   self.t=setTimeout("poll()",3000);
-    // }
+    clearTimeout(self.t);
   }
+
 })
