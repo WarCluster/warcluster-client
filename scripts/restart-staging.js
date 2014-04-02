32c4a701415cd9ad8678ca7532e8d0a9e74f9455
Vitaliy Filipov <altras@gmail.com>
2014-04-02 22:35:03 +0300

    0.0.78 release

--- scripts/restart-staging.js
+++ scripts/restart-staging.js
@@ -1,2 +1,2 @@
 require('shelljs/global');
-exec('ssh -p 7022 owl@kiril.eu "cd ~/warcluser-site; git pull origin master; forever restart /home/owl/warcluster-site/warcluster-staging.js"')
+exec('ssh -p 7022 owl@kiril.eu "cd ~/warcluster-site; git pull origin master; forever restart /home/owl/warcluster-site/warcluster-staging.js"')
