6ef362af0d35ae4a27f386d7413c51a0befae033
Vitaliy Filipov <altras@gmail.com>
2014-04-18 01:48:30 +0300

    refactor the animation flow

--- client/warcluster/views/leaderboard/index.js
+++ client/warcluster/views/leaderboard/index.js
@@ -1,7 +1,7 @@
 var individualRender = jadeCompile(require("./render/individual.jade"));
 var teamRender = jadeCompile(require("./render/team.jade"));
 
-//TODO: need to refactor this ajax shit O_O
+//TODO: need to refactor this whole view with the ajax shit O_O
 
 module.exports = Backbone.View.extend({
   template: jadeCompile(require("./index.jade")),
@@ -97,42 +97,32 @@ module.exports = Backbone.View.extend({
   },
   populateIndividual: function(data) {
     var _that = this;
+
     for(var i=0;i<=data.length-1;i++) {
-      //animate the shit out of it!
+      //animate only when necessary
       if (jQuery.isEmptyObject(this.cache)) {
         $("tbody tr:nth-child(" + (i+1) + ") > .twitter-username").html("<a href='https://twitter.com/"+data[i].Username+"' target='_blank'>@"+data[i].Username+"</a>");
         $("tbody tr:nth-child(" + (i+1) + ") > .race-color").css({"background": "rgb("+ parseInt(data[i].Team.R*255)+","+parseInt(data[i].Team.G*255) +","+parseInt(data[i].Team.B*255)+")"});
         $("tbody tr:nth-child(" + (i+1) + ") > .home-planet").html(data[i].HomePlanet);
         $("tbody tr:nth-child(" + (i+1) + ") > .planets").html(data[i].Planets);
       } 
-      // else if (this.cache[i].Username === data[i].Username) {
-      //   //don't animate
-      //   $("tbody tr:nth-child(" + (i+1) + ") > .planets").html(data[i].Planets);
-      // } 
+      else if (this.cache[i].Username === data[i].Username && this.chache[i].Planets !== data[i].Planets) {
+        //TODO: Shake it dat ass 
+        $("tbody tr:nth-child(" + (i+1) + ") > .planets").html(data[i].Planets);
+      } 
       else {
         _that.iter = $("tbody tr:nth-child(" + (i+1) + ")");
-        _that.iter.animate({deg: 60}, {
-          duration: 200,
-          step: function(now) {
-            _that.iter.css({
-              transform: "rotateX(" + now*1.5 + "deg)" 
-            })
-          }
-        });
+        //asynchronous animations are slower than the iteration of the 'for' - that's why I need to bind the calls
+        _.bind(this.implodeAnimation,_that.iter);
+        this.implodeAnimation(_that.iter);
 
         $("tbody tr:nth-child(" + (i+1) + ") > .twitter-username").html("<a href='https://twitter.com/"+data[i].Username+"' target='_blank'>@"+data[i].Username+"</a>");
         $("tbody tr:nth-child(" + (i+1) + ") > .race-color").css({"background": "rgb("+ parseInt(data[i].Team.R*255)+","+parseInt(data[i].Team.G*255) +","+parseInt(data[i].Team.B*255)+")"});
         $("tbody tr:nth-child(" + (i+1) + ") > .home-planet").html(data[i].HomePlanet);
         $("tbody tr:nth-child(" + (i+1) + ") > .planets").html(data[i].Planets);
         
-        _that.iter.animate({deg: 0}, {
-          duration: 300,
-          step: function(now) {
-            _that.iter.css({
-              transform: "rotateX(" + now*1.5 + "deg)" 
-            })
-          }
-        });
+        _.bind(this.explodeAnimation,_that.iter);
+        this.explodeAnimation(_that.iter);
       }
     }
     this.cache = data;
@@ -144,17 +134,59 @@ module.exports = Backbone.View.extend({
   populateTeams: function(data) {
     var _that = this;
     for(var i=0;i<data.length;i++) {
-      //animate the shit out of it!
-      $("tbody tr:nth-child(" + (i+1) + ") > .race-color").css({"background": "rgb("+ parseInt(data[i].Color.R*255)+","+parseInt(data[i].Color.G*255) +","+parseInt(data[i].Color.B*255)+")"});
-      $("tbody tr:nth-child(" + (i+1) + ") > .race-color").html(data[i].Name);
-      $("tbody tr:nth-child(" + (i+1) + ") > .players-number").html(data[i].Players);
-      $("tbody tr:nth-child(" + (i+1) + ") > .planets-number").html(data[i].Planets);
+      if (jQuery.isEmptyObject(this.cache)) {
+        $("tbody tr:nth-child(" + (i+1) + ") > .race-color").css({"background": "rgb("+ parseInt(data[i].Color.R*255)+","+parseInt(data[i].Color.G*255) +","+parseInt(data[i].Color.B*255)+")"});
+        $("tbody tr:nth-child(" + (i+1) + ") > .race-color").html(data[i].Name);
+        $("tbody tr:nth-child(" + (i+1) + ") > .players-number").html(data[i].Players);
+        $("tbody tr:nth-child(" + (i+1) + ") > .planets-number").html(data[i].Planets);
+      }
+      else if (this.cache[i].Name === data[i].Name && (this.cache[i].Players !== data[i].Players || this.cache[i].Planets !== data[i].Planets)) {
+        //TODO: Shake it dat ass 
+        $("tbody tr:nth-child(" + (i+1) + ") > .planets").html(data[i].Planets);
+      }
+      else {
+        _that.iter = $("tbody tr:nth-child(" + (i+1) + ")");
+        //asynchronous animations are slower than the iteration of the 'for' - that's why I need to bind the calls
+        _.bind(this.implodeAnimation,_that.iter);
+        this.implodeAnimation(_that.iter);
+
+        $("tbody tr:nth-child(" + (i+1) + ") > .twitter-username").html("<a href='https://twitter.com/"+data[i].Username+"' target='_blank'>@"+data[i].Username+"</a>");
+        $("tbody tr:nth-child(" + (i+1) + ") > .race-color").css({"background": "rgb("+ parseInt(data[i].Team.R*255)+","+parseInt(data[i].Team.G*255) +","+parseInt(data[i].Team.B*255)+")"});
+        $("tbody tr:nth-child(" + (i+1) + ") > .home-planet").html(data[i].HomePlanet);
+        $("tbody tr:nth-child(" + (i+1) + ") > .planets").html(data[i].Planets);
+        
+        _.bind(this.explodeAnimation,_that.iter);
+        this.explodeAnimation(_that.iter);
+      }
     }
+    this.cache = data;
     this.leaderboardAjaxTimeout = setTimeout(function() {
       _.bind(_that.pollTeams,_that);
       _that.pollTeams();
     }, 2500);
+  },
+  //TODO: find a proper naming for the animation. Figure out a better UX animation
+  implodeAnimation: function(element) {
+    debugger;
+    console.log(element);
+    element.animate({deg: 60}, {
+      duration: 200,
+      step: function(now) {
+        element.css({
+          transform: "rotateX(" + now*1.5 + "deg)" 
+        })
+      }
+    });
+  },
+  explodeAnimation: function(element) {
+    console.log(element);
+    element.animate({deg: 0}, {
+      duration: 300,
+      step: function(now) {
+        element.css({
+          transform: "rotateX(" + now*1.5 + "deg)" 
+        })
+      }
+    });
   }
-
-
 })
