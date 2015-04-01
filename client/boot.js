3a0d95132676278f76538749bffd647e0290412e
Vitaliy Filipov <altras@gmail.com>
2015-04-01 21:56:07 +0300

    Revert "update greensock tweenlite"
    This reverts commit 4261c25e0083e4757a448703cd76deacb9e01d4f.
    
    Conflicts:
    	client/boot.js
    	client/warcluster/views/leaderboard/index.js

--- client/boot.js
+++ client/boot.js
@@ -1,6 +1,9 @@
 // require("../client/vendor");
 require("../client/vendor/jquery");
 require("../client/vendor/bootstrap");
+require("../client/vendor/bootstrap-fileupload.min");
+require("../client/vendor/bootstrap-colorselector");
+require("../client/vendor/google-code-prettify/prettify");
 require("../client/vendor/TweenLite.min");
 require("../client/vendor/plugins/jquery-mousewheel.min")($);
 require("../client/vendor/plugins/CSSPlugin.min");
